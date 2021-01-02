0dd9f3638444026a76e41b7c5dbed3bf1fbb8a20
kanishka19s07012 <kanishkadharmawardana11976@gmail.com>
2021-01-02 22:50:53 +0530

    final system

---
+++ contactus.php
@@ -0,0 +1,46 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+    
+  <div id="content" style="color:white; padding: 1px 50px 150px;">
+    <img src="images/contactpic.jpg" alt="contact image" style="width:95%">
+    <h2><span style="color:white"> Contact Us</span></h2>
+    <table width="100%" height="152" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td bgcolor="#BCE0A8" style="color:black"><span class="style10">Head Office</span></td>
+        <td bgcolor="#BCE0A8" style="color:black"><span class="style10">Branch Office</span></td>
+      </tr>
+      <tr>
+        <td>Moose Head office<br/></td>
+        <td>Moose showroom<br/>
+          
+<br/></td>
+      </tr>
+      <tr>
+        <td>Phone: 0112848476</td>
+        <td>Phone: 0754724052</td>
+      </tr>
+      <tr>
+        <td>Email : mooseshopping@gmail.com</td>
+        <td>Email : mooseshopping@gmail.com</td>
+
+      </tr>
+			<td>Website: <a href="#">www.moose.com</a> </td>
+			<td>Website: <a href="#">www.moose.com</a> </td>
+    </table>
+  
+    <p>&nbsp;</p>
+  </div>
+    
+
+
+
+    <?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer" style="bottom:0">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 Brought to You By Moose</a></strong>
+    </div>
+</footer>
